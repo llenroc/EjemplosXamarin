7440c273775dec5bcbe4962a28288d9ec9ebd99d
Esteban Solano Granados <stvansolano@gmail.com>
2017-09-05 10:01:47 -0600

    Xamarin Effects

---
+++ HolaMoviles/HolaMoviles.iOS/Effects/ClearEntryEffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Effects")]
+[assembly: ExportEffect(typeof(HolaMoviles.iOS.Effects.ClearEntryEffect), "ClearEntryEffect")]
+namespace HolaMoviles.iOS.Effects
+{
+	public class ClearEntryEffect : PlatformEffect
+	{
+		protected override void OnAttached()
+		{
+			ConfigureControl();
+		}
+
+		protected override void OnDetached()
+		{
+		}
+
+		private void ConfigureControl()
+		{
+			((UITextField)Control).ClearButtonMode = UITextFieldViewMode.WhileEditing;
+		}
+	}
+}
---
+++ HolaMoviles/HolaMoviles/Effects/ClearEntryEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HolaMoviles
+{
+	public class ClearEntryEffect : RoutingEffect
+	{
+		public ClearEntryEffect() : base("Effects.ClearEntryEffect")
+		{
+		}
+	}
+}
